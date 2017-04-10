7033a36459f0e5266f07c69f10349c70597340a1
wulang8353 <284823698@qq.com>
2017-04-10 22:12:54 +0800

    fix alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,8 +22,9 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'], // 可以在require引入的模块路径中补全文件后缀
     alias: {                              // 通过别名去缩短模块路径整个字符串长度
       'vue$': 'vue/dist/vue.esm.js',
+      'src': path.resolve(__dirname, '../src'),
       'common': path.resolve(__dirname, '../src/common'),
-      '@': resolve('src'),
+      'components': path.resolve(__dirname, '../src/components')
     }
   },
   module: {
